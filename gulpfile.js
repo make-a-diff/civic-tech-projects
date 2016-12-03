5d2190e0018e3882f6bd034969c5025771955e3e
interglobalmedia <interglobalmedia@gmail.com>
2016-12-03 14:50:37 -0500

    Update 2016-12-03T19:50:31.576Z

--- gulpfile.js
+++
@@ -1,6 +0,0 @@
-require('./gulp/tasks/styles');
-require('./gulp/tasks/watch');
-require('./gulp/tasks/scripts');
-require('./gulp/tasks/modernizr');
-require('./gulp/tasks/build');
-require('./gulp/tasks/deploy');

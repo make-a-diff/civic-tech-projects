8b6511f8038ebe8ec91ff9c2101c8ed4498bc35d
interglobalmedia <interglobalmedia@gmail.com>
2016-12-03 15:17:22 -0500

    Added imagemin for image minification. Ran new build.

--- gulp/tasks/build.js
+++ gulp/tasks/build.js
@@ -1,4 +1,5 @@
 var gulp = require('gulp'),
+imagemin = require('gulp-imagemin'),
     del = require('del'),
     usemin = require('gulp-usemin'),
     rev = require('gulp-rev'),
@@ -23,6 +24,7 @@ gulp.task('copyGeneralFiles', ['deleteDistFolder'], function() {
     var pathsToCopy = [
         './app/**/*',
         '!./app/index.html',
+        '!./app/assets/images/**',
         '!./app/assets/styles/**',
         '!./app/assets/scripts/**',
         '!./app/temp',
@@ -33,7 +35,21 @@ gulp.task('copyGeneralFiles', ['deleteDistFolder'], function() {
         .pipe(gulp.dest('./dist'));
 });
 
-gulp.task('usemin', ['deleteDistFolder', 'styles', 'scripts'], function() {
+gulp.task('optimizeImages', ['deleteDistFolder'], function() {
+    return gulp.src('./app/assets/images/**/*')
+        .pipe(imagemin({
+            progressive: true,
+            interlaced: true,
+            multipass: true
+        }))
+        .pipe(gulp.dest('./dist/assets/images'));
+});
+
+gulp.task('useminTrigger', ['deleteDistFolder'], function() {
+    gulp.start('usemin');
+})
+
+gulp.task('usemin', ['styles', 'scripts'], function() {
     return gulp.src('./app/index.html')
         .pipe(usemin({
             css: [function() {return rev()}, function() {return cssnano()}],
@@ -42,4 +58,4 @@ gulp.task('usemin', ['deleteDistFolder', 'styles', 'scripts'], function() {
         .pipe(gulp.dest('./dist'));
 });
 
-gulp.task('build', ['deleteDistFolder', 'copyGeneralFiles', 'usemin']);
+gulp.task('build', ['deleteDistFolder', 'copyGeneralFiles', 'optimizeImages', 'useminTrigger']);
